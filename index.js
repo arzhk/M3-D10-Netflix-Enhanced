4441b48502194136f2d6a68bf02a9f46c634ae42
arzhk <72197750+arzhk@users.noreply.github.com>
2020-11-15 22:32:51 +0000

    Added functionality
    - Added content to the API
    - Added content generation by genre to the homepage
    - Added ability to filter summary table by genre on the dashboard
    - Added a reset to all fields in the dashboard upon submitting any form
    - Improved editing flow so that genre is automatically pulled upon entering an ID

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const fetchShowData = async (endpoint) => {
       });
       const data = await response.json();
       allGenres = [...data];
-      allGenres.sort();
+      allGenres.sort().shift();
       return allGenres;
     }
   } catch (error) {
@@ -149,25 +149,45 @@ async function fetchAllMovies() {
   allMovies = (await Promise.all(promiseArray)).flat();
 }
 
-function dashboard_loadSummaryTable(array) {
+function resetAllFields() {
+  console.log("fired");
+  const allInputs = document.querySelectorAll("input");
+  allInputs.forEach((e) => (e.value = ""));
+
+  const allTextAreas = document.querySelectorAll("textarea");
+  allTextAreas.forEach((e) => (e.value = ""));
+
+  const allSelects = document.querySelectorAll("select");
+  allSelects.forEach((e) => (e.value = ""));
+}
+
+function dashboard_loadSummaryTable(array = allMovies) {
   const tableContainer = document.querySelector("#summary-table-body");
 
   tableContainer.innerHTML = "";
 
-  allMovies.forEach((e) => {
+  array.forEach((e) => {
     const newTableItem = document.createElement("tr");
     newTableItem.innerHTML = `
         <th scope="row">${e.name}</th>
             <td>${e.category}</td>
             <td>${e._id}</td>
             <td class="text-right">
-            <button id="edit-this-element" class="btn btn-secondary d-inline-block mr-2" disabled>Edit</button>
+            <button id="edit-this-element" class="btn btn-warning d-inline-block mr-2">Edit</button>
             <button id="delete-this-element" class="btn btn-danger">Delete</button>
         </td>`;
-
     tableContainer.appendChild(newTableItem);
+    newTableItem.querySelector("#edit-this-element").addEventListener("click", () => setEditData(event));
     newTableItem.querySelector("#delete-this-element").addEventListener("click", () => removeShowData(e._id));
   });
+}
+
+function filterTable() {
+  const filterSelect = document.querySelector("#filterBy-genre").value;
+
+  filterSelect === "All Movies"
+    ? dashboard_loadSummaryTable(allMovies)
+    : dashboard_loadSummaryTable(allMovies.filter((e) => e.category === filterSelect));
 }
 
 function setModalData(show) {
@@ -177,8 +197,7 @@ function setModalData(show) {
   document.querySelector("#modal-show-img").value = show[0].imageUrl;
 }
 
-async function loadModal(e) {
-  e.preventDefault();
+async function loadModal() {
   showId = document.querySelector("#inputShowID-edit").value;
   showGenre = document.querySelector("#inputShowGenre-edit").value;
 
@@ -199,6 +218,17 @@ async function loadModal(e) {
   isFirstEdit = false;
 }
 
+function setEditData(event) {
+  const target = event.currentTarget.parentNode.parentNode;
+  const targetID = target.querySelector("td:nth-child(3)").innerText;
+  const editIDBox = document.querySelector("#inputShowID-edit");
+
+  editIDBox.value = targetID;
+
+  findGenre();
+  loadModal();
+}
+
 function findGenre() {
   const editIDBox = document.querySelector("#inputShowID-edit");
   const genreBox = document.querySelector("#inputShowGenre-edit");
@@ -215,12 +245,80 @@ function findGenre() {
         genreBox.value = e.category;
       }
     });
+  } else {
+    genreBox.value = "";
   }
 }
+
+/* Home functions */
+
+function createShowCard(show) {
+  const newShowCard = document.createElement("div");
+  newShowCard.classList.add("show-wrapper", "mr-1");
+  newShowCard.innerHTML = `
+    <img class="show-img" src="${show.imageUrl}"/>
+    <div class="show-content d-flex justify-content-center align-items-start flex-column px-4 py-2">
+    <h5 class="mb-2">${show.name}</h5>
+    <p class="mb-2">${show.description}?</p>
+    <button><i class="fas fa-play pl-1 pr-2 py-2"></i>PLAY</button>
+    </div>`;
+
+  return newShowCard;
+}
+
+function home_loadGenreSelections() {
+  const selector = document.querySelector("#genre-filter");
+  allGenres.forEach((e) => {
+    const newOption = document.createElement("option");
+    newOption.setAttribute("value", `${e}`);
+    newOption.innerText = e;
+    selector.appendChild(newOption);
+  });
+}
+
+function home_loadContent() {
+  const contentWrapper = document.querySelector("#content-wrapper");
+  allGenres.forEach(async (e) => {
+    const newGenreRow = document.createElement("div");
+    newGenreRow.classList.add("row", "pt-5");
+    newGenreRow.innerHTML = `
+    <h2 class="d-block w-100">${e}</h2>
+    <div id="${e}-row" class="shows-row">
+    </div>`;
+
+    const genreRow = newGenreRow.querySelector(`#${e}-row`);
+    const filteredMovieArray = allMovies.filter((movie) => movie.category === e);
+    filteredMovieArray.forEach(async (e) => {
+      const newShow = await createShowCard(e);
+      genreRow.appendChild(newShow);
+    });
+
+    contentWrapper.appendChild(newGenreRow);
+  });
+}
+
+function home_loadElements() {
+  home_loadGenreSelections();
+  home_loadContent();
+}
+
+const startHome = async () => {
+  await fetchShowData();
+  await fetchAllMovies();
+  home_loadElements(allGenres);
+};
+
+/*---------------------------------------------------*/
+/* Dashboard functions */
 
 function postNewShow(e) {
   e.preventDefault();
   postShowData();
+}
+
+function editShow(e) {
+  e.preventDefault();
+  loadModal();
 }
 
 function deleteShow(e) {
@@ -231,7 +329,10 @@ function deleteShow(e) {
 }
 
 const startDashboard = async () => {
+  resetAllFields();
   await fetchShowData();
   await fetchAllMovies();
-  dashboard_loadSummaryTable(allGenres);
+  dashboard_loadSummaryTable();
 };
+
+/*---------------------------------------------------*/
